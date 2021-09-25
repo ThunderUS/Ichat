707d2edb82f8e9ce9525ccb82cef8efef8945e67
Anton <thunders.job@gmail.com>
2021-09-24 21:43:28 -0400

    created node.js server (server.js) install:      socket.io,      cors,      express,      nodemon

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import server from"http";
+import {Server} from"socket.io";
+import cors from "cors";
+
+const app=express();
+const webServer=server.createServer(app);
+const IO= new Server(webServer);
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/reg",(req,res)=>{
+  res.send("fffffffff");
+  console.log("good");
+});
+app.post("/reg",(req,res)=>{
+  console.log(req.body);
+  res.send();
+});
+
+app.listen(9999,(err)=>{
+  if(err){
+    console.log(err);
+  } else {
+    console.log("working");
+  }
+});
