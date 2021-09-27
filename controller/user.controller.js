6058b1a68dc5023fe8f05b9dbd758f7881419a05
Anton <thunders.job@gmail.com>
2021-09-26 21:49:10 -0400

    Created DB connection
    Created Registration User.
    add tables
    
    created user.controller.js function created user

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,9 +1,13 @@
+import pool from "../db.js";
 class UserController{
     async createUser(req,res){
         const {name,surname,login,password}=req.body;
-        console.log(name,surname,login,password)
-        res.json("ok");
-        //todo finish DB
+
+        const newPerson= await pool.query("INSERT INTO users (name, surname,login) values ($1,$2,$3) RETURNING * ",[name,surname,login]);
+        //console.log(newPerson["rows"][0]["id"]);
+        await pool.query("INSERT INTO aups (aups,user_id) values ($1,$2) RETURNING *",[password,newPerson["rows"][0]["id"]]);
+        res.json(newPerson);
+
     }
     async getLoginsUsers(req,res){
 
---
+++ db.js
@@ -0,0 +1,11 @@
+import  db from "pg";
+const Pool=db.Pool;
+const pool= new Pool({
+  user:"postgres",
+  password:"1234",
+  host:"localhost",
+  port:5432,
+  database:"ichat",
+});
+
+export default pool;
