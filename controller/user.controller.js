641abecfb281115483e67719d357f957d32c619e
Anton <thunders.job@gmail.com>
2021-09-27 21:10:43 -0400

    Server work
    put security to data in CreateUser.

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,13 +1,14 @@
 import pool from "../db.js";
 class UserController{
     async createUser(req,res){
+        console.log("enter")
         const {name,surname,login,password}=req.body;
-
-        const newPerson= await pool.query("INSERT INTO users (name, surname,login) values ($1,$2,$3) RETURNING * ",[name,surname,login]);
-        //console.log(newPerson["rows"][0]["id"]);
-        await pool.query("INSERT INTO aups (aups,user_id) values ($1,$2) RETURNING *",[password,newPerson["rows"][0]["id"]]);
-        res.json(newPerson);
-
+        if(name && login && surname && password){
+            const newPerson= await pool.query("INSERT INTO users (name, surname,login) values ($1,$2,$3) RETURNING * ",[name,surname,login]);
+            await pool.query("INSERT INTO aups (aups,user_id) values ($1,$2) RETURNING *",[password,newPerson["rows"][0]["id"]]);
+            res.json(newPerson);
+        }
+        res.status(500).send("error");
     }
     async getLoginsUsers(req,res){
 
