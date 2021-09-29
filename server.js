f0d3982d9272f46ed03be15fedaca4190647e5f0
Anton <thunders.job@gmail.com>
2021-09-29 13:33:38 -0400

    Server work.
    add try/catch for errors, in main file server.js.

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ import cors from "cors";
 import userControl from "./controller/user.controller.js"
 import Log from "./server/log.js";
 
-
 const PORT = process.env.PORT || 8080;
-
 const app = express();
 //const webServer = server.createServer(app);
 //const IO = new Server(webServer);
@@ -15,15 +13,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+try {
+  app.post("/login", userControl.loginUser);
+  app.get("/user/list", userControl.getLoginsUsers);
+  app.post("/user", userControl.createUser);
+} catch (e) {
+  Log.setLog(`${e}`);
+}
 
-app.post("/login", userControl.loginUser);
-app.get("/user/list", userControl.getLoginsUsers);
-app.post("/user", userControl.createUser);
 
 
 app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
+    Log.setLog(`${err}`);
   } else {
     Log.setLog(`Server start on ${PORT}`);
   }
