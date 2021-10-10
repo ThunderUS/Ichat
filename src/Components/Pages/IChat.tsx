c440a64c471da3065f67ac820d992d723c0ce2b9
Anton <thunders.job@gmail.com>
2021-10-10 11:35:09 -0400

    APP work
    adaptive for (max-width: 770px) , (max-height: 721px)

--- src/Components/Pages/IChat.tsx
+++ src/Components/Pages/IChat.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import "../../style/IChat.scss"
 import Chats from "../moduls/IChat/Chats";
+import LoginInfo from "../moduls/IChat/LoginInfo";
+import Rooms from "../moduls/IChat/Rooms";
+import Sender from "../moduls/IChat/Sender";
 
 
 function IChat() {
     return (
         <div className={"IChat"}>
-            <Chats/>
+            <div className="IChat_left">
+                <LoginInfo/>
+                <Rooms/>
+            </div>
+            <div className="IChat_right">
+                <Chats/>
+                <Sender/>
+            </div>
         </div>
     );
 }
