5be00b559bb83488db3908e7ba36810df5453f89
Anton <thunders.job@gmail.com>
2021-10-23 19:48:14 -0400

    prepare for HEROKU db connection test

--- server/db.js
+++ server/db.js
@@ -1,12 +1,19 @@
-import  db from "pg";
+import db from "pg";
 
-const Pool=db.Pool;
-const pool= new Pool({
-  user:"postgres",
-  password:"1234",
-  host:"localhost",
-  port:5432,
-  database:"ichat",
+const Pool = db.Pool;
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "1234",
+//   host: "localhost",
+//   port: 5432,
+//   database: "ichat",
+// });
+
+const pool = new Pool({
+  user: "kzfajvjggkhiey",
+  password: "e969bdce6f568662be9f0a91c66e3ac610098988b4deffa562377145df84aa93",
+  host: "ec2-52-3-79-87.compute-1.amazonaws.com",
+  port: 5432,
+  database: "ddc539biiorft6",
 });
-
 export default pool;
